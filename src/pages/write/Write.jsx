74540b979085d2ea13e05b600be36aa883f9ac22
Gladys <gladys.ni@yahoo.fr>
2022-04-22 14:58:59 +0200

    updated

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -17,7 +17,7 @@ const Create = () => {
 		};
 		try {
 			await axios.post("https://mern-gladys.herokuapp.com/api/ticket/", newTicket);
-			window.location.replace("https://mern-gladys.herokuapp.com/api/");
+			window.location.replace("https://mern-gladys.herokuapp.com/api/ticket/all-tickets");
 		} catch (error) {
 			console.log(error.response.data);
 		}
